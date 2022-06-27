3f5edda8a5cd5aa42282585dbb2e2372e996061d
Petar <petar.harambasa@racunarstvo.hr>
2022-06-27 17:03:03 +0200

    Serach, filter, sort opcije implementirane na tablicu s apartmanima

--- RWAProjekt/MVC/App_Start/BundleConfig.cs
+++ RWAProjekt/MVC/App_Start/BundleConfig.cs
@@ -12,13 +12,15 @@ namespace MVC.App_Start
         {
             bundles.Add(new StyleBundle("~/Content").Include(
                  "~/Content/bootstrap.min.css",
-                 "~/Content/lightbox.css"
+                 "~/Content/lightbox.css",
+                 "~/Content/DataTables/css/jquery.dataTables.css"
              ));
 
             bundles.Add(new ScriptBundle("~/Scripts").Include(
                 "~/Scripts/jquery-3.6.0.js",
                 "~/Scripts/bootstrap.js",
-                "~/Scripts/lightbox-2.6.js"
+                "~/Scripts/lightbox-2.6.js",
+                "~/Scripts/DataTables/jquery.dataTables.js"
             ));
         }
     }
